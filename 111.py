cd860c89f91b594eae7dd341ad6ca07afc23d48c
Konrad Listwan-Ciesielski <konrad.listwan@gmail.com>
2011-10-09 18:34:03 -0400

    Solved 112

--- 111.py
+++ 111.py
@@ -3,7 +3,7 @@
 Find the sum of all S(10, d)."""
 
 import prime
-from itertools import ifilter, permutations, repeat, izip
+from itertools import ifilter, permutations, repeat, izip, product
 
 def M(n=10, d=None):
     return dict(izip(xrange(n), [8,9,8,9,9,9,9,9,8,9]))
@@ -23,17 +23,30 @@ def N(d,n=10):
                 if prime.isprime(cur_num):
                     primes.append(cur_num)
             to_permute[i] = str(d)
-        return len(primes)
+        return primes
     if dict[d] == n-2:
         to_permute = list(repeat(str(d),n)) #['1','1','1','1']
         digits = [str(k) for k in range(10) if k!=d]
-        
+        double_digits = list(product(digits,digits))
+        current_permute = int("".join(to_permute))
+        if prime.isprime(current_permute): primes.append(current_permute)
+        for i in range(n-1):
+            for j in range(1,n):
+                for pair in double_digits:
+                    to_permute[i] = pair[0]
+                    to_permute[j] = pair[1]
+                    cur_num = int("".join(to_permute))
+#                    print "on",cur_num
+                    if prime.isprime(cur_num): 
+ #                       print cur_num,"PRIME"
+                        primes.append(cur_num)
+                to_permute[i] = str(d)
+                to_permute[j] = str(d)
+        return [k for k in primes if len(str(k))==n]
 
-
-
-    return 69
-
+result = 0
 for i in range(0,10):
-    print i,N(i)
-#    break
+    result += sum(N(i))
 
+
+print result
